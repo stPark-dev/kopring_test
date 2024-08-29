8d049f54240c43a2e01e433a5cdd8be074aeddd2
stpark <dmc6618@gractor.com>
2024-08-29 09:54:46 +0900

    remove user service repository entity

--- src/main/kotlin/com/example/demo/repository/UserRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.demo.repository
-
-import com.example.demo.entity.User
-import org.springframework.data.repository.CrudRepository
-
-interface UserRepository :CrudRepository<User, Long>
